0dfed8429e9d49758bc1fadbc30408c0a5220c59
CharlesMorel <charles.morel@viacesi.fr>
2019-01-23 15:52:15 +0100

    Event Presentation page added (responsive)

--- PHP_Server/resources/views/event.blade.php
+++ PHP_Server/resources/views/event.blade.php
@@ -5,7 +5,7 @@
 <div>
     <div class="event">
         <div class="img_right">
-            <img src="/img/events_img/img_event1.jpg" alt="Photo de l'exia party">
+            <img src="/img/events_img/img1_event1.jpg" alt="Photo de l'exia party">
         </div>
         <div class="description_left">
             <div class="bloc">
@@ -13,14 +13,14 @@
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                     Accusantium autem, qui, aperiam porro cum omnis possimus cumque unde adipisci nemo provident
                     asperiores sit animi consequuntur iste suscipit ad fuga maxime.</p>
-                <a href="event_presentation">LIRE LA SUITE</a>
+                <button class="read_more" href="event_presentation">LIRE LA SUITE</button>
             </div>
         </div>
     </div>
 
     <div class="event">
         <div class="img_left">
-            <img src="/img/events_img/img_event2.jpg" alt="Photo de foot">
+            <img src="/img/events_img/img1_event2.jpg" alt="Photo de foot">
         </div>
         <div class="description_right">
             <div class="bloc">
@@ -28,14 +28,14 @@
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                     Accusantium autem, qui, aperiam porro cum omnis possimus cumque unde adipisci nemo provident
                     asperiores sit animi consequuntur iste suscipit ad fuga maxime.</p>
-                <a href="event_presentation">LIRE LA SUITE</a>
+                <button class="read_more" href="event_presentation">LIRE LA SUITE</button>
             </div>
         </div>
     </div>
 
     <div class="event">
         <div class="img_right">
-            <img src="/img/events_img/img_event1.jpg" alt="Photo de l'exia party">
+            <img src="/img/events_img/img1_event1.jpg" alt="Photo de l'exia party">
         </div>
         <div class="description_left">
             <div class="bloc">
@@ -43,14 +43,14 @@
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                     Accusantium autem, qui, aperiam porro cum omnis possimus cumque unde adipisci nemo provident
                     asperiores sit animi consequuntur iste suscipit ad fuga maxime.</p>
-                <a href="event_presentation">LIRE LA SUITE</a>
+                <button class="read_more" href="event_presentation">LIRE LA SUITE</button>
             </div>
         </div>
     </div>
 
     <div class="event">
         <div class="img_left">
-            <img src="/img/events_img/img_event2.jpg" alt="Photo de foot">
+            <img src="/img/events_img/img1_event2.jpg" alt="Photo de foot">
         </div>
         <div class="description_right">
             <div class="bloc">
@@ -58,7 +58,7 @@
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                     Accusantium autem, qui, aperiam porro cum omnis possimus cumque unde adipisci nemo provident
                     asperiores sit animi consequuntur iste suscipit ad fuga maxime.</p>
-                <a href="event_presentation">LIRE LA SUITE</a>
+                <button class="read_more" href="event_presentation">LIRE LA SUITE</button>
             </div>
         </div>
     </div>
